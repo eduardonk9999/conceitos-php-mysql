3c50f6b76f60c3f428180436f42339b87d11f97c
eduardonk9999 <thiago.novak7@gmail.com>
2021-04-02 09:54:42 -0300

    Add, loopss

---
+++ src/arraysmultidimensionais.php
@@ -0,0 +1,17 @@
+<?php
+  $blog = [
+    ['title' => 'mario partty', 'mario', 'lorem', 30],
+    ['title' => 'mario kart cheats', 'toad', 'lorem', 25],
+    ['title' => 'zelda hidden chests', 'link', 'lorem, 50']
+  ];  
+?>
+
+<html>
+  <head>
+    <title>Multidimensional Arrays</title>
+  </head>
+
+  <body>
+   
+  </body>
+</html>
---
+++ src/loops.php
@@ -0,0 +1,27 @@
+<?php
+
+  $ninjas = ['shaun', 'ryu', 'yoshi'];
+  
+  for($i = 0; $i < count($ninjas); $i++) {
+    echo $ninjas[$i] . '<br/>';
+  }
+
+  foreach($ninjas as $ninja) {
+    echo $ninja . '<br />'
+  }
+
+  $products = [
+    
+  ]
+
+?>
+
+<html>
+  <head>
+    <title>Loops</title>
+  </head>
+
+  <body>
+   
+  </body>
+</html>
